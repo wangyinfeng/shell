3058dec32425eaec498fff4a186850b5657f4482
wangyinfeng <batman.wang@qq.com>
2013-05-07 13:54:46 +0800

    Update and rename image.sh to ftp_du.sh

--- ftp_du.sh
+++ ftp_du.sh
@@ -6,18 +6,18 @@ USER1='yfwang'
 PASSWD1='blade123'
 USER2='ftpuser'
 PASSWD2='ftpuser'
-FILE='gstkc'
+FILE=$1
 
 ftp -v -n $HOST1 <<END_SCRIPT
 quote USER $USER1
 quote PASS $PASSWD1
 cd temp/leo
 binary
-get gstkc
+get ${FILE}
 bye
 END_SCRIPT
 echo
-echo "Download file $FILE from $HOST1"
+echo "Download file ${FILE} from $HOST1"
 echo
 
 ftp -v -n $HOST2 <<END_SCRIPT
@@ -25,9 +25,9 @@ quote USER $USER2
 quote PASS $PASSWD2
 cd /tftpboot
 binary
-put gstkc
+put ${FILE}
 bye
 END_SCRIPT
 echo
-echo "Upload file $FILE to $HOST2"
+echo "Upload file ${FILE} to $HOST2"
 echo
