9ec88cb5c0637824a24d8ea83618ba7aeb055602
wangyinfeng <wang.batman@gmail.com>
2013-09-03 20:39:53 +0800

    upload to github

---
+++ clear.bat
@@ -0,0 +1,20 @@
+@echo off
+echo 正在清除系统垃圾文件，请稍等......
+del /f /s /q %systemdrive%\*.tmp
+del /f /s /q %systemdrive%\*._mp
+del /f /s /q %systemdrive%\*.log
+del /f /s /q %systemdrive%\*.gid
+del /f /s /q %systemdrive%\*.chk
+del /f /s /q %systemdrive%\*.old
+del /f /s /q %systemdrive%\recycled\*.*
+del /f /s /q %windir%\*.bak
+del /f /s /q %windir%\prefetch\*.*
+rd /s /q %windir%\temp & md %windir%\temp
+REM del /f /q %userprofile%\cookies\*.*
+del /f /q %userprofile%\recent\*.*
+del /f /s /q "%userprofile%\Local Settings\Temporary Internet Files\*.*"
+del /f /s /q "%userprofile%\Local Settings\Temp\*.*"
+del /f /s /q "%userprofile%\recent\*.*"
+del /f /s /q c:\program files\Tencent\qq\cache\*.*
+echo 清除完成！
+echo. & pause
