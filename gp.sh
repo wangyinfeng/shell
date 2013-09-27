d34a0dbe8836d5a28bebef4ad9323cac627867e4
wangyinfeng <wang.batman@gmail.com>
2013-09-27 13:42:13 +0800

    break lines

--- gp.sh
+++ gp.sh
@@ -21,7 +21,7 @@ do
          echo "     gp -w STRING   exact math";
          echo "     gp -i STRING   no case sensitivity";
          exit 1;;  
-      w) precious="y" ; shift; break;;
+      w) precious="y" ; shift; break;;  
       i) case_sensitivity="n"; shift; break;;
       *|?) echo "Invalid parameter"; exit 1;;
     esac
@@ -44,7 +44,13 @@ fi
 echo "Find [$1] in $PWD ..."
 echo 
 #find . -name "*.c" -o -name "*.h*" | xargs grep -r "$1" | grep -v "grep"
-find . \( -path */lib/bcmsdk_4.2.4 -o -path '*/lib/bcmsdk_5.2.*' -o -path '*/lib/bcmsdk_5.3.*' -o -path '*/lib/bcmsdk_5.4.*' -o -path '*/lib/bcmsdk_5.5.*' -o -path '*/lib/bcmsdk_5.6.*' -o -path '*/lib/bcmsdk5.5.*' -o -path '*/lib/bcmsdk_5.7.*' -o -path */*/lib/bcmsdk_4.2.4 -o -path '*/*/lib/bcmsdk_5.2.*' -o -path '*/*/lib/bcmsdk_5.3.*' -o -path '*/*/lib/bcmsdk_5.4.*' -o -path '*/*/lib/bcmsdk_5.5.*' -o -path '*/*/lib/bcmsdk_5.6.*' -o -path '*/*/lib/bcmsdk5.5.*' -o -path */*/lib/bcmsdk_5.7.0 \) -prune -o \( -name "*.c" -o -name "*.h*" -o -name "*.clive" \) | xargs grep -sn $grep_para "$1" | awk '{ print } END { print "\nMatch number:\t " NR }'
+find . \( -path '*/lib/bcmsdk_4.2.4' -o -path '*/lib/bcmsdk_5.2.*' -o -path '*/lib/bcmsdk_5.3.*' \
+        -o -path '*/lib/bcmsdk_5.4.*' -o -path '*/lib/bcmsdk_5.5.*' -o -path '*/lib/bcmsdk_5.6.*' \
+        -o -path '*/lib/bcmsdk5.5.*' -o -path '*/lib/bcmsdk_5.7.*' -o -path '*/*/lib/bcmsdk_4.2.4' \
+        -o -path '*/*/lib/bcmsdk_5.2.*' -o -path '*/*/lib/bcmsdk_5.3.*' -o -path '*/*/lib/bcmsdk_5.4.*' \
+        -o -path '*/*/lib/bcmsdk_5.5.*' -o -path '*/*/lib/bcmsdk_5.6.*' -o -path '*/*/lib/bcmsdk5.5.*' \
+        -o -path '*/*/lib/bcmsdk_5.7.0' -o -path '*/lib/bcmsdk_5.10.1' -o -path '*/lib/bcmsdk_6.2.1_ea' \
+        -o -path '*/lib/bcmsdk_6.*' \) -prune -o \( -name "*.c" -o -name "*.h*" -o -name "*.clive" \) | xargs grep -sn $grep_para "$1" | awk '{ print } END { print "\nMatch number:\t " NR }'
 #find . -name "*.c" -o -name "*.h*" | xargs grep -sn "$1"
 # -s silent Not show error message 
 # -n line-number Show match line number
